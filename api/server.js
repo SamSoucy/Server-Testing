1b95cbf90c45988b0a355d52ffff6ca86ab20318
Samsoucy <newf930@outlook.com>
2019-03-14 22:44:35 -0400

    changed endpoints and tests. Now working

--- api/server.js
+++ api/server.js
@@ -1,54 +1,29 @@
 const express = require('express');
 const server = express();
-const users = require('../users/usersModel');
 server.use(express.json());
 
-server.get('/', (req,res) => {
-    res.status(200).json("message: working");
-});
- 
-server.get('/users', (req,res) => {
-    res.status(200).json({name: "Kim", age: 32, occupation: "student"});
-});
- 
-server.post('/users', (req,res) => {
-    const user = req.body;
-    const name = user.name;
-    const age = user.age;
-    const occupation = user.occupation;
-    if(!name) res.status(422).json({message: "name is missing"});
-    if(!age) res.status(422).json({message: "age is missing"});;
-    if(!occupation) res.status(422).json({message:"occupation is missing"});;
-    users.insert(user)
-         .then( ids => {
-            console.log(ids);
-            res.status(201).json(ids);
-         })
-         .catch(err => {
-            res.status(500).json({err:"Cannot add a user at this time"});
-         })
-});
 
-server.delete("/users/:id", (req, res) => {
-    const { id } = req.params;
-    if (!id) res.status(404).json({ message: "no id" });
-    users.findById(id)
-        .then(userId => {
-            if(userId[0]) {
-                users.remove(userId[0])
-                    .then(ids => {
-                        res.status(200).json({ message: "user is deleted" });
-                    })
-                    .catch(err => {
-                        res.status(500).json({ message: "Something went wrong" });
-                    });
-                } else {
-                   res.status(404).json({message:"There is no user with this id"});
-                }      
-        })
-        .catch(error => {
-            res.status(500).json({error: "There is something wrong the server"});
-        });
+server.get('/', (req, res) => {
+    res.status(200).json({ Message: "working" });
 })
-        
-module.exports = server;
+
+server.get("/user", (req, res) => {
+    
+})
+
+
+server.post('/greet', (req, res) => {
+    if(req.body) {
+        res.status(200).json({Hello : `${req.body.firstName} ${req.body.lastName}`})
+    } else {
+        res.status(400).json({message : "Bad request.."});
+}
+
+
+server.delete('/user/:id', (req, res) => {
+        const id = req.params.id;
+        res.status(202).json({ message: "Post has been deleted" })
+    })
+   
+}) 
+module.exports = server;  
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,43 +1,56 @@
-const req = require('supertest');
+
+const request = require('supertest');
 const server = require('./server.js');
 
-describe("route handler", () => {
-    it("should return 200 ok", async () => {
-        const res = await req(server).get("/");
-            expect(res.status).toBe(200);
-    });
+
+
+describe('server.js', () => {
+    describe('/route', () => {
+        it('should return status code 200', async () => {
+            let response = await request(server).get('/');
+            expect(response.status).toBe(200);
+        })
+        it('should get response in JSON format..', () => {
+            request(server)
+                .get('/')
+                .then(response => {
+                    expect(response.type).toBe('application/json')    
+                })    
+        })
+        it('should return with a body like: {Message: "working"', async () => {
+            let response = await request(server).get('/');
+            expect(response.body).toEqual({Message: "working"});
+        });
+    })
     
-    it("should respond with list of users", async () => {
-        const userArray = { "name": "Kim", "age": 32, "occupation": "student" };
-        const res = await req(server).get("/users");
-        expect(res.body).toEqual(userArray);
-    });
-})
+    describe('POST/greet endpoint', () => {
+        it('should greet the person', async () => {
+            let response = await request(server)
+                .post('/greet')
+                .send({ firstName : "Kim", lastName : "Soucy"});    
+            expect(response.body).toEqual({Hello : "Kim Soucy"});
+        })
+        it('should get response in JSON format..', () => {
+            request(server)
+                .post('/greet')
+                .then(response => {
+                    expect(response.type).toBe('application/json')    
+                })    
+        })
+    })  
 
-describe('Post("/")', () => {
-    it("should respond with a success code of 201 when creating a new user", async () => {
-        const user = { "name": "Kim", "age": 32, "occupation": "student" };
-        const res = await req(server).post("/users").send(user);
-        expect(res.status).toBe(201);
-    });
+    
+    describe("delete users", () => {
+        it('should delete a user', async() => {
+            let response = await request(server)
+                .delete('/user/:id');                 
+            expect(response.status).toBe(202);
+        });
 
-    it("should responde with 422 error code when a name is not entered", async () => {
-        const user = { "name": "", "age": 32, "occupation": "student" };
-        const res = await req(server).post("/users").send(user);
-        expect(res.status).toBe(422);
-    });
-});
-
-describe("delete users", () => {
-    it("should respond with 200 success code when a user has been deleted", async () => {
-        const id = 1;
-        const res = await req(server).delete("/users/:id");
-        expect(res.status).toBe(200);
-        
-    });
-    it("should respond with a json", async () => {
-        const res = await req(server).delete('/users/id');
-        expect(res.type).toEqual('application/json');
-  });
-});
+        it("delete should return JSON", async () => {
+            const response = await request(server).delete('/user/:id');
+            expect(response.type).toEqual('application/json');
+        });
+    })  
+}) 
 
